f2ef8f48fc7012490038464a3c86d6d1b6a8c458
Yo581515 <88982075+Yo581515@users.noreply.github.com>
2021-10-14 14:56:03 +0200

    HelloPer

---
+++ Frivillig2/src/main/java/PPAPServlet/ServletClasse.java
@@ -0,0 +1,42 @@
+package PPAPServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servilet")
+public class ServletClasse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String navn = request.getParameter("navn");
+		
+		response.setContentType("text/html; charset=ISO-8859-1");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("<title>Hallo</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hallo "+navn+"!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+	}
+
+
+
+}
