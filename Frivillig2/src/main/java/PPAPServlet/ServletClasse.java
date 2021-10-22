4067796e09ad1ed966c958518d76c62c7f6d30ac
581515 <47955690+Y581515@users.noreply.github.com>
2021-10-22 13:30:49 +0200

    ferdig oppg2 i guess :(

--- Frivillig2/src/main/java/PPAPServlet/ServletClasse.java
+++ Frivillig2/src/main/java/PPAPServlet/ServletClasse.java
@@ -9,19 +9,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/servilet")
 public class ServletClasse extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private String escapeHtml(String s) {
+		String resultat = s;
+		resultat = resultat.replaceAll("<", "&lt;");
+		resultat = resultat.replaceAll(">", "&gt;");
+		resultat = resultat.replaceAll("\"", "&quot;");
+		resultat = resultat.replaceAll(" ", "&nbsp;");
+		return resultat;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String navn = request.getParameter("navn");
-		
+		navn = escapeHtml(navn);
+
 		response.setContentType("text/html; charset=ISO-8859-1");
 		PrintWriter out = response.getWriter();
-		
+
+		String hello = request.getHeader("accept-language").substring(0, 2).equals("en") ? "hello" : "hallo";
+
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
@@ -29,14 +40,10 @@ public class ServletClasse extends HttpServlet {
 		out.println("<title>Hallo</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Hallo "+navn+"!");
+		out.println("<h1>" + hello + "&nbsp;" + navn + "!</h1>");
 		out.println("</body>");
 		out.println("</html>");
-		
-		
-		
+
 	}
-
-
 
 }
